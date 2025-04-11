47e365576573f658261c214fcbe1549baa02b21a
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2025-04-11 14:29:00 +0200

    refa: remove duplicates

--- extnode/common.go
+++ extnode/common.go
@@ -3,9 +3,42 @@
 
 package extnode
 
+import (
+	"github.com/steadybit/action-kit/go/action_kit_api/v2"
+	"github.com/steadybit/extension-kit/extutil"
+)
+
 const (
 	NodeTargetType         = "com.steadybit.extension_kubernetes.kubernetes-node"
 	DrainNodeActionId      = "com.steadybit.extension_kubernetes.drain_node"
 	TaintNodeActionId      = "com.steadybit.extension_kubernetes.taint_node"
 	NodeCountCheckActionId = "com.steadybit.extension_kubernetes.node_count_check"
+)
+
+var (
+	targetSelectionTemplates = action_kit_api.TargetSelection{
+		TargetType: NodeTargetType,
+		SelectionTemplates: extutil.Ptr([]action_kit_api.TargetSelectionTemplate{
+			{
+				Label:       "host name",
+				Description: extutil.Ptr("Find node by its name"),
+				Query:       "host.hostname=\"\"",
+			},
+			{
+				Label:       "deployment",
+				Description: extutil.Ptr("Find node by cluster, namespace and deployment."),
+				Query:       "k8s.cluster-name=\"\" and k8s.namespace=\"\" and k8s.deployment=\"\"",
+			},
+			{
+				Label:       "statefulset",
+				Description: extutil.Ptr("Find node by cluster, namespace and statefulset."),
+				Query:       "k8s.cluster-name=\"\" and k8s.namespace=\"\" and k8s.statefulset=\"\"",
+			},
+			{
+				Label:       "daemonset",
+				Description: extutil.Ptr("Find node by cluster, namespace and daemonset."),
+				Query:       "k8s.cluster-name=\"\" and k8s.namespace=\"\" and k8s.daemonset=\"\"",
+			},
+		}),
+	}
 )
--- extpod/common.go
+++ extpod/common.go
@@ -1,7 +1,35 @@
 package extpod
 
+import (
+	"github.com/steadybit/action-kit/go/action_kit_api/v2"
+	"github.com/steadybit/extension-kit/extutil"
+)
+
 const (
 	PodTargetType     = "com.steadybit.extension_kubernetes.kubernetes-pod"
 	DeletePodActionId = "com.steadybit.extension_kubernetes.delete_pod"
 	CrashLoopActionId = "com.steadybit.extension_kubernetes.crash_loop_pod"
+)
+
+var (
+	targetSelectionTemplates = action_kit_api.TargetSelection{
+		TargetType: PodTargetType,
+		SelectionTemplates: extutil.Ptr([]action_kit_api.TargetSelectionTemplate{
+			{
+				Label:       "deployment",
+				Description: extutil.Ptr("Find pods by cluster, namespace and deployment"),
+				Query:       "k8s.cluster-name=\"\" AND k8s.namespace=\"\" AND k8s.deployment=\"\"",
+			},
+			{
+				Label:       "statefulset",
+				Description: extutil.Ptr("Find pods by cluster, namespace and statefulset."),
+				Query:       "k8s.cluster-name=\"\" and k8s.namespace=\"\" and k8s.statefulset=\"\"",
+			},
+			{
+				Label:       "daemonset",
+				Description: extutil.Ptr("Find pods by cluster, namespace and daemonset."),
+				Query:       "k8s.cluster-name=\"\" and k8s.namespace=\"\" and k8s.daemonset=\"\"",
+			},
+		}),
+	}
 )
