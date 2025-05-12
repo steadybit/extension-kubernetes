93339fa2bb518e55881b02a8d892c0d0902b3fee
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2025-05-12 11:25:15 +0200

    fix: do not discover ingress for role based because of ingress class

--- main.go
+++ main.go
@@ -98,7 +98,7 @@ func main() {
 		action_kit_sdk.RegisterAction(extdaemonset.NewDaemonSetPodCountCheckAction(client.K8S))
 	}
 
-	if !extconfig.Config.DiscoveryDisabledIngress && client.K8S.Permissions().IsModifyIngressAllowed() {
+	if !extconfig.Config.DiscoveryDisabledIngress && client.K8S.Permissions().IsModifyIngressAllowed() && !extconfig.IsUsingRoleBasedAccessControl(){
 		discovery_kit_sdk.Register(extingress.NewIngressDiscovery(client.K8S))
 		action_kit_sdk.RegisterAction(extingress.NewHAProxyBlockTrafficAction())
 		action_kit_sdk.RegisterAction(extingress.NewHAProxyDelayTrafficAction())
