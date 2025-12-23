12ed0a1db389209666e8f67f1591b4ba611d6fd5
Jay Hankins <jhankins@salesforce.com>
2025-12-23 12:23:50 -0500

    fix: use selector param

--- client/client.go
+++ client/client.go
@@ -179,7 +179,9 @@ func (l *rolloutLister) List(selector labels.Selector) ([]*unstructured.Unstruct
 	result := make([]*unstructured.Unstructured, 0, len(items))
 	for _, item := range items {
 		if obj, ok := item.(*unstructured.Unstructured); ok {
-			result = append(result, obj)
+			if selector.Matches(labels.Set(obj.GetLabels())) {
+				result = append(result, obj)
+			}
 		}
 	}
 	return result, nil
@@ -205,7 +207,9 @@ func (l *rolloutNamespaceLister) List(selector labels.Selector) ([]*unstructured
 	result := make([]*unstructured.Unstructured, 0, len(items))
 	for _, item := range items {
 		if obj, ok := item.(*unstructured.Unstructured); ok {
-			result = append(result, obj)
+			if selector.Matches(labels.Set(obj.GetLabels())) {
+				result = append(result, obj)
+			}
 		}
 	}
 	return result, nil
