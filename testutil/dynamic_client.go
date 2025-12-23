ff8ce66849aa220aa0bc5e1a34f096a17f3e1704
Jay Hankins <jhankins@salesforce.com>
2025-12-23 14:31:01 -0500

    refactor: support multiple gvks

--- testutil/dynamic_client.go
+++ testutil/dynamic_client.go
@@ -7,19 +7,42 @@ import (
 	"k8s.io/client-go/dynamic/fake"
 )
 
-// NewFakeDynamicClient creates a fake dynamic client with additional custom types (i.e. Argo Rollouts)
-func NewFakeDynamicClient() *fake.FakeDynamicClient {
-	scheme := runtime.NewScheme()
-	gvk := schema.GroupVersionKind{
-		Group:   "argoproj.io",
-		Version: "v1alpha1",
-		Kind:    "Rollout",
-	}
+// registerGVK registers a GroupVersionKind  in the scheme
+func registerGVK(scheme *runtime.Scheme, gvk schema.GroupVersionKind) {
 	scheme.AddKnownTypeWithName(gvk, &unstructured.Unstructured{})
-	scheme.AddKnownTypeWithName(schema.GroupVersionKind{
-		Group:   gvk.Group,
-		Version: gvk.Version,
-		Kind:    gvk.Kind + "List",
-	}, &unstructured.UnstructuredList{})
+}
+
+// NewFakeDynamicClient creates a fake dynamic client with additional custom types.
+// If no GVKs are provided, it defaults to Argo Rollouts for backward compatibility.
+// To add additional types, pass them as arguments:
+//
+//	NewFakeDynamicClient(
+//		schema.GroupVersionKind{Group: "argoproj.io", Version: "v1alpha1", Kind: "Rollout"},
+//		schema.GroupVersionKind{Group: "example.com", Version: "v1", Kind: "CustomResource"},
+//	)
+func NewFakeDynamicClient(gvks ...schema.GroupVersionKind) *fake.FakeDynamicClient {
+	scheme := runtime.NewScheme()
+
+	// Default to Argo Rollout  if no GVKs provided
+	if len(gvks) == 0 {
+		gvks = []schema.GroupVersionKind{
+			{
+				Group:   "argoproj.io",
+				Version: "v1alpha1",
+				Kind:    "Rollout",
+			},
+			{
+				Group:   "argoproj.io",
+				Version: "v1alpha1",
+				Kind:    "RolloutList",
+			},
+		}
+	}
+
+	// Register all provided GVKs
+	for _, gvk := range gvks {
+		registerGVK(scheme, gvk)
+	}
+
 	return fake.NewSimpleDynamicClient(scheme)
 }
